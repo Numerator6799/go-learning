66b0f90d9e44f80ccacd9417eb9de423c3a90cfa
Numerator6799 <metalgoat88@proton.me>
2024-06-23 17:10:57 -0300

    sample type that asks for interface

--- interfaces/main.go
+++ interfaces/main.go
@@ -10,6 +10,8 @@ type englishBot struct{}
 
 type spanishBot struct{}
 
+type russianBot struct{ bot }
+
 // just because those types implement [getGreeting() string] function,
 // then they are considered bots
 func (englishBot) getGreeting() string {
@@ -30,4 +32,10 @@ func main() {
 
 	sb := spanishBot{}
 	printGreeting(sb)
+
+	// this will break because the type does not implement getGreeting()
+	// but notice I could tell the compiler russianBot type is bot interface.
+	// I think this is better
+	rb := russianBot{}
+	printGreeting(rb)
 }
